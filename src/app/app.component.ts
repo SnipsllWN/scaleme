229888e1e35516111b8a9804a68562a200ac1239
Tümmers, David <david.tuemmers@connext.de>
2024-03-26 14:33:16 +0100

    ohne dir-scaleme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {QuestionComponent} from "./question/question.component";
 import {ScalemeComponent} from "./scaleme/scaleme.component";
-import {LoginComponent} from "./dir-scaleme/pages/login/login.component";
-import {RouteService} from "./dir-scaleme/route.service";
-import {RegisterComponent} from "./dir-scaleme/pages/register/register.component";
-import {NanswerComponent} from "./dir-scaleme/pages/nanswer/nanswer.component";
-import {NquestionComponent} from "./dir-scaleme/pages/nquestion/nquestion.component";
-import {RequestsComponent} from "./dir-scaleme/pages/requests/requests.component";
-import {RequestComponent} from "./dir-scaleme/pages/request/request.component";
+import {LoginComponent} from "./pages/login/login.component";
+import {RouteService} from "./route.service";
+import {RegisterComponent} from "./pages/register/register.component";
+import {NanswerComponent} from "./pages/nanswer/nanswer.component";
+import {NquestionComponent} from "./pages/nquestion/nquestion.component";
+import {RequestsComponent} from "./pages/requests/requests.component";
+import {RequestComponent} from "./pages/request/request.component";
 
 @Component({
   selector: 'app-root',
